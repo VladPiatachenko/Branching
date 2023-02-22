8ec09750511349a3485bee40bdcedec5c819ef91
Erlkonig <vl.piatachenko@cs.sumdu.edu.ua>
2023-02-22 10:42:24 +0200

    added content 2

--- branching/src/main/java/org/krams/tutorial/branching/Branching.java
+++ branching/src/main/java/org/krams/tutorial/branching/Branching.java
@@ -13,5 +13,6 @@ public class Branching {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Goodbye World!");
+           System.out.println("welcome World!");
     }
 }
